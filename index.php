d834a61ed20ff38910bc1c3d16d007a0d70e3a71
Jose Pumarejo <jose.pumarejo02@gmail.com>
2023-01-06 16:50:06 -0500

    mostrando imagenes y la opcion de carrito solo cuado se haya inicado sesion

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 while ($filas = mysqli_fetch_array($query)) {
                 ?>
                     <div class="producto">
-                        <!-- <img src="./productos/imagenproductos/<?php echo $filas['nombrearchivo']; ?>" class="img-fluid " alt="Foto de producto" style="width: 180px ;height:180px"> -->
+                        <img src="./productos/imagenproductos/<?php echo $filas['nombrearchivo']; ?>" class="img-fluid " alt="Foto de producto" style="width: 180px ;height:180px">
                         <hr>
                         <div class="form-producto">
                             <script>
@@ -53,12 +53,20 @@
                                     });
                                 }
                             </script>
-                            <button onclick="detallesproducto(<?php echo $filas['idproducto'] ?>)" id="agregaracarrito" class="btn btn-success" data-toggle="modal" data-target="#formulariocarrito">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
-                                    <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
-                                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                </svg>
-                            </button>
+                            <?php
+                            if (isset($_SESSION['cedula'])) {
+                            ?>
+                                <button onclick="detallesproducto(<?php echo $filas['idproducto'] ?>)" id="agregaracarrito" class="btn btn-success" data-toggle="modal" data-target="#formulariocarrito">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                        <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
+                                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                                    </svg>
+                                </button>
+                            <?php
+                            } else {
+                            }
+                            ?>
+
                         </div>
 
                     </div>
